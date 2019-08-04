f119413de5bafac1dfc82b588da2978e4b1e81de
rossiemk <tulk2aku@yahoo.com>
2019-08-04 15:52:59 +0100

    commit1

---
+++ assets/handler.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler(); 
+
+$validator = $pp->getValidator();
+$validator->fields(['name','email','phone','number','Date and time','Message'])->areRequired()->maxLength(6000);
+$validator->field('email')->isEmail();
+$validator->field('message')->maxLength(6000);
+
+
+
+
+$pp->sendEmailTo(['info@GGEventhall.com', 'tulk2aku22@gmail.com']); // ← Your email here
+
+echo $pp->process($_POST);
