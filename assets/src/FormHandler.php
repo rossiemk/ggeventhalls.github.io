8fdaa1e20a91dda0a5b1c721fcde64dc01c2713e
rossiemk <tulk2aku@yahoo.com>
2019-08-08 04:37:05 +0100

    7a

--- assets/src/FormHandler.php
+++ assets/src/FormHandler.php
@@ -46,7 +46,7 @@ class FormHandler
 
 		$host = isset($_SERVER['SERVER_NAME'])?$_SERVER['SERVER_NAME']:'localhost';
         $from_email ='forms@'.$host;
-   		$this->mailer->setFrom('http://ggeventhall.com','Contact Form',false);  
+   		$this->mailer->setFrom('http://ggeventhalls.com','Contact Form',false);  
 
    		$this->captcha = false;   
 
