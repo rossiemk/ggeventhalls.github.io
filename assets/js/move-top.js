d3e2eff985d2b48b5fb149e517d4c8a2100f12f8
rossiemk <tulk2aku@yahoo.com>
2019-08-17 05:09:24 +0100

    8k

--- assets/js/move-top.js
+++ assets/js/move-top.js
@@ -1,18 +1,18 @@
+function Animate() {
+ 
+  $(".scroll-down").addClass("animated  infinite bounce");
+  
+   $(".scroll-to-top").delay(3000).addClass("animated shake");
+}
 
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function() {scrollFunction()};
-
-	function scrollFunction() {
-	  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		document.getElementById("myBtn").style.display = "block";
-	  } else {
-		document.getElementById("myBtn").style.display = "none";
-	  }
-	}
-
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-	  document.body.scrollTop = 0;
-	  document.documentElement.scrollTop = 0;
-	}
-	
+  // scrolling to links on page
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  });
+});
